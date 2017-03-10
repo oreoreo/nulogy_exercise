e73bd088f0a41c43957f83b579065d326dc3eb8e
oreoreo <catherinechen1002@hotmail.com>
2017-03-09 23:14:30 -0500

    basic functionality working! but no string parsing yet so it's not the most elegant solution

--- markup.py
+++ markup.py
@@ -3,6 +3,10 @@ import math
 def markup(price, numPeople, category):
    #multiply the markup with flat -> (people + categories)
    #price after flat markup 
-   categories = ["drugs", "food", "electronics"]
+   categories = {"drugs":0.075, "food":0.13, "electronics":0.02}
    flat_price = price * 1.05
+   if category in categories:
+       second_markup = 1.0 + categories[category] + 0.012 * numPeople
+       final_price = flat_price * second_markup
+       return round(final_price, 2)
    
